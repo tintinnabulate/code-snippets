e27feb034492f4090a381da94d505790aff496c4
Tintinnabulate <tintinn@abulate>
2018-10-24 14:45:39 +0100

    Add logging

--- go/closed-loop/server.py
+++ go/closed-loop/server.py
@@ -1,33 +1,20 @@
+import logging
+import logging.config
 import signal
 from socket import socket, AF_INET, SOCK_DGRAM
 from threading import Thread
 
-def signal_handler(signal, frame):
-    global interrupted
-    interrupted = True
-
-signal.signal(signal.SIGINT, signal_handler)
-
-interrupted = False
-
-def safe_print(content):
-    '''
-    >>> import dis
-    >>> def foo(x): print x
-    >>> dis.dis(foo) # PRINT_NEWLINE is a separate instruction
-    '''
-    print "{0}\n".format(content),
-
 def server(address):
     sock = socket(AF_INET, SOCK_DGRAM)
     sock.bind(address)
+    logger = logging.getLogger()
     while True:
         msg, addr = sock.recvfrom(8192)
-        safe_print(str(addr) + " : " + str(msg))
-        if interrupted:
-            break
+        # safe_print(str(addr) + " : " + str(msg))
+        logger.debug(str(addr) + " : " + str(msg))
 
 if __name__ == '__main__':
+    logging.config.fileConfig('logging_config.ini')
     host = ''
     ports = [2561, 2563, 2564, 2565, 2569]
     threads = []
