894727bb5780c7005bb19f4a45f97bcce5026a4a
Tintinnabulate <tintinn@abulate>
2018-10-24 15:11:31 +0100

    JSON logging output

--- go/closed-loop/server.py
+++ go/closed-loop/server.py
@@ -1,8 +1,21 @@
+import json
 import logging
 import logging.config
 import signal
 from socket import socket, AF_INET, SOCK_DGRAM
 from threading import Thread
+
+class StructuredMessage(object):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def __str__(self):
+        return '%s' % (json.dumps(self.kwargs),)
+
+_ = StructuredMessage   # optional, to improve readability
+
+# logging.basicConfig(level=logging.INFO, format='%(message)s')
+# logging.info(_('message 1', foo='bar', bar='baz', num=123, fnum=123.456))
 
 def server(address):
     sock = socket(AF_INET, SOCK_DGRAM)
@@ -11,7 +24,7 @@ def server(address):
     while True:
         msg, addr = sock.recvfrom(8192)
         # safe_print(str(addr) + " : " + str(msg))
-        logger.debug("%s : %s", addr, msg)
+        logger.debug(_(addr=addr, msg=msg))
 
 if __name__ == '__main__':
     logging.config.fileConfig('logging_config.ini')
